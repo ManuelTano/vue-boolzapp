bc5a3c2e7686c2ee24464c1053c254cf92894d1b
ManuelTano <manueltano98@gmail.com>
2022-07-15 16:37:50 +0200

    Milestone 3 - Aggiunta del messaggio

--- js/script.js
+++ js/script.js
@@ -32,255 +32,267 @@ const root = new Vue({
         newMessage: '',
         contacts: [
             {
-              name: 'Michele',
-              avatar: '_1',
-              visible: true,
-              messages: [{
-                date: '15/07/2022 15:30:55',
-                text: 'Hai portato a spasso il cane?',
-                status: 'sent'
-              },
-              {
-                date: '15/07/2022 15:50:00',
-                text: 'Ricordati di dargli da mangiare',
-                status: 'sent'
-              },
-              {
-                date: '15/07/2022 16:15:22',
-                text: 'Tutto fatto!',
-                status: 'received'
-              }
-              ],
+                name: 'Michele',
+                avatar: '_1',
+                visible: true,
+                messages: [{
+                    date: '15/07/2022 15:30:55',
+                    text: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                },
+                {
+                    date: '15/07/2022 15:50:00',
+                    text: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                },
+                {
+                    date: '15/07/2022 16:15:22',
+                    text: 'Tutto fatto!',
+                    status: 'received'
+                }
+                ],
             },
             {
-              name: 'Fabio',
-              avatar: '_2',
-              visible: true,
-              messages: [{
-                date: '16/07/2022 16:30:00',
-                text: 'Ciao come stai?',
-                status: 'sent'
-              },
-              {
-                date: '16/07/2022 16:30:55',
-                text: 'Bene grazie! Stasera ci vediamo?',
-                status: 'received'
-              },
-              {
-                date: '16/07/2022 16:35:00',
-                text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                status: 'sent'
-              }
-              ],
+                name: 'Fabio',
+                avatar: '_2',
+                visible: true,
+                messages: [{
+                    date: '16/07/2022 16:30:00',
+                    text: 'Ciao come stai?',
+                    status: 'sent'
+                },
+                {
+                    date: '16/07/2022 16:30:55',
+                    text: 'Bene grazie! Stasera ci vediamo?',
+                    status: 'received'
+                },
+                {
+                    date: '16/07/2022 16:35:00',
+                    text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                    status: 'sent'
+                }
+                ],
             },
             {
-              name: 'Samuele',
-              avatar: '_3',
-              visible: true,
-              messages: [{
-                date: '15/07/2022 10:10:40',
-                text: 'La Marianna va in campagna',
-                status: 'received'
-              },
-              {
-                date: '15/07/2022 10:20:10',
-                text: 'Sicuro di non aver sbagliato chat?',
-                status: 'sent'
-              },
-              {
-                date: '15/07/2022 16:15:22',
-                text: 'Ah scusa!',
-                status: 'received'
-              }
-              ],
+                name: 'Samuele',
+                avatar: '_3',
+                visible: true,
+                messages: [{
+                    date: '15/07/2022 10:10:40',
+                    text: 'La Marianna va in campagna',
+                    status: 'received'
+                },
+                {
+                    date: '15/07/2022 10:20:10',
+                    text: 'Sicuro di non aver sbagliato chat?',
+                    status: 'sent'
+                },
+                {
+                    date: '15/07/2022 16:15:22',
+                    text: 'Ah scusa!',
+                    status: 'received'
+                }
+                ],
             },
             {
-              name: 'Jack',
-              avatar: '_4',
-              visible: true,
-              messages: [{
-                date: '15/07/2022 15:30:55',
-                text: 'Lo sai che ha aperto una nuova pizzeria?',
-                status: 'sent'
-              },
-              {
-                date: '15/07/2022 15:50:00',
-                text: 'Si, ma preferirei andare al cinema',
-                status: 'received'
-              }
-              ],
+                name: 'Jack',
+                avatar: '_4',
+                visible: true,
+                messages: [{
+                    date: '15/07/2022 15:30:55',
+                    text: 'Lo sai che ha aperto una nuova pizzeria?',
+                    status: 'sent'
+                },
+                {
+                    date: '15/07/2022 15:50:00',
+                    text: 'Si, ma preferirei andare al cinema',
+                    status: 'received'
+                }
+                ],
             },
             {
                 name: 'Davide',
                 avatar: '_5',
                 visible: true,
                 messages: [{
-                  date: '15/07/2022 15:30:55',
-                  text: 'Hai portato a spasso il cane?',
-                  status: 'sent'
+                    date: '15/07/2022 15:30:55',
+                    text: 'Hai portato a spasso il cane?',
+                    status: 'sent'
                 },
                 {
-                  date: '15/07/2022 15:50:00',
-                  text: 'Ricordati di dargli da mangiare',
-                  status: 'sent'
+                    date: '15/07/2022 15:50:00',
+                    text: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
                 },
                 {
-                  date: '15/07/2022 16:15:22',
-                  text: 'Tutto fatto!',
-                  status: 'received'
+                    date: '15/07/2022 16:15:22',
+                    text: 'Tutto fatto!',
+                    status: 'received'
                 }
                 ],
-              },
-              {
+            },
+            {
                 name: 'Alessia',
                 avatar: '_6',
                 visible: true,
                 messages: [{
-                  date: '16/07/2022 16:30:00',
-                  text: 'Ciao come stai?',
-                  status: 'sent'
+                    date: '16/07/2022 16:30:00',
+                    text: 'Ciao come stai?',
+                    status: 'sent'
                 },
                 {
-                  date: '16/07/2022 16:30:55',
-                  text: 'Bene grazie! Stasera ci vediamo?',
-                  status: 'received'
+                    date: '16/07/2022 16:30:55',
+                    text: 'Bene grazie! Stasera ci vediamo?',
+                    status: 'received'
                 },
                 {
-                  date: '16/07/2022 16:35:00',
-                  text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                  status: 'sent'
+                    date: '16/07/2022 16:35:00',
+                    text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                    status: 'sent'
                 }
                 ],
-              },
-              {
+            },
+            {
                 name: 'Alex',
                 avatar: '_7',
                 visible: true,
                 messages: [{
-                  date: '16/07/2022 10:10:40',
-                  text: 'La Marianna va in campagna',
-                  status: 'received'
+                    date: '16/07/2022 10:10:40',
+                    text: 'La Marianna va in campagna',
+                    status: 'received'
                 },
                 {
-                  date: '16/07/2022 10:20:10',
-                  text: 'Sicuro di non aver sbagliato chat?',
-                  status: 'sent'
+                    date: '16/07/2022 10:20:10',
+                    text: 'Sicuro di non aver sbagliato chat?',
+                    status: 'sent'
                 },
                 {
-                  date: '16/07/2022 16:15:22',
-                  text: 'Ah scusa!',
-                  status: 'received'
+                    date: '16/07/2022 16:15:22',
+                    text: 'Ah scusa!',
+                    status: 'received'
                 }
                 ],
-              },
-              {
+            },
+            {
                 name: 'U Boss',
                 avatar: '_8',
                 visible: true,
                 messages: [{
-                  date: '16/07/2022 15:30:55',
-                  text: 'Lo sai che ha aperto una nuova pizzeria?',
-                  status: 'sent'
+                    date: '16/07/2022 15:30:55',
+                    text: 'Lo sai che ha aperto una nuova pizzeria?',
+                    status: 'sent'
                 },
                 {
-                  date: '16/07/2022 15:50:00',
-                  text: 'Si, ma preferirei andare al cinema',
-                  status: 'received'
+                    date: '16/07/2022 15:50:00',
+                    text: 'Si, ma preferirei andare al cinema',
+                    status: 'received'
                 }
                 ],
-              },
-              {
+            },
+            {
                 name: 'Alberto',
                 avatar: '_3',
                 visible: true,
                 messages: [{
-                  date: '15/07/2022 10:10:40',
-                  text: 'La Marianna va in campagna',
-                  status: 'received'
+                    date: '15/07/2022 10:10:40',
+                    text: 'La Marianna va in campagna',
+                    status: 'received'
                 },
                 {
-                  date: '15/07/2022 10:20:10',
-                  text: 'Sicuro di non aver sbagliato chat?',
-                  status: 'sent'
+                    date: '15/07/2022 10:20:10',
+                    text: 'Sicuro di non aver sbagliato chat?',
+                    status: 'sent'
                 },
                 {
-                  date: '15/07/2022 16:15:22',
-                  text: 'Ah scusa!',
-                  status: 'received'
+                    date: '15/07/2022 16:15:22',
+                    text: 'Ah scusa!',
+                    status: 'received'
                 }
                 ],
-              },
-              {
+            },
+            {
                 name: 'Thomas',
                 avatar: '_4',
                 visible: true,
                 messages: [{
-                  date: '15/07/2022 15:30:55',
-                  text: 'Lo sai che ha aperto una nuova pizzeria?',
-                  status: 'sent'
+                    date: '15/07/2022 15:30:55',
+                    text: 'Lo sai che ha aperto una nuova pizzeria?',
+                    status: 'sent'
                 },
                 {
-                  date: '15/07/2022 15:50:00',
-                  text: 'Si, ma preferirei andare al cinema',
-                  status: 'received'
+                    date: '15/07/2022 15:50:00',
+                    text: 'Si, ma preferirei andare al cinema',
+                    status: 'received'
                 }
                 ],
-              },
-              {
-                  name: 'Paolo',
-                  avatar: '_5',
-                  visible: true,
-                  messages: [{
+            },
+            {
+                name: 'Paolo',
+                avatar: '_5',
+                visible: true,
+                messages: [{
                     date: '15/07/2022 15:30:55',
                     text: 'Hai portato a spasso il cane?',
                     status: 'sent'
-                  },
-                  {
+                },
+                {
                     date: '15/07/2022 15:50:00',
                     text: 'Ricordati di dargli da mangiare',
                     status: 'sent'
-                  },
-                  {
+                },
+                {
                     date: '15/07/2022 16:15:22',
                     text: 'Tutto fatto!',
                     status: 'received'
-                  }
-                  ],
-                },
-                {
-                  name: 'Desirè',
-                  avatar: '_6',
-                  visible: true,
-                  messages: [{
+                }
+                ],
+            },
+            {
+                name: 'Desirè',
+                avatar: '_6',
+                visible: true,
+                messages: [{
                     date: '16/07/2022 16:30:00',
                     text: 'Ciao come stai?',
                     status: 'sent'
-                  },
-                  {
+                },
+                {
                     date: '16/07/2022 16:30:55',
                     text: 'Bene grazie! Stasera ci vediamo?',
                     status: 'received'
-                  },
-                  {
+                },
+                {
                     date: '16/07/2022 16:35:00',
                     text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                     status: 'received'
-                  }
-                  ],
-                },
-          ]
-            },
-            computed: {
-
-            },
-            methods: {
-                setCurrentContact(index) {
-                    this.currentIndex = index;
-                },
-                sendMessage() {
-
                 }
+                ],
+            },
+        ]
+    },
+    computed: {
+
+    },
+    methods: {
+        setCurrentContact(index) {
+            this.currentIndex = index;
+        },
+        sendMessage() {
+            const myMessage = {
+                date: "17/07/2022 17:30:55",
+                text: this.newMessage,
+                status: "sent",
+            };
+
+            const yoursMessage = {
+                date: "17/07/2022 17:30:55",
+                text: 'Okay!',
+                status: "received",
             }
+
+            this.contacts[this.currentIndex].messages.push(myMessage);
+        }
+    }
 });
 
 
